5ff89c50a302d0339f08ce84c0d4a582776c6473
Your Name <vivienne.haferkamp@gmail.com>
2022-03-12 11:36:52 +0100

    Server class

--- app/src/main/java/com/example/myapplication/Client.java
+++ app/src/main/java/com/example/myapplication/Client.java
@@ -11,20 +11,15 @@ public class Client {
         try{
             Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
             DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream()); //we convert the raw data into stream
-            BufferedReader inputStream = new BufferedReader(new InputStreamReader(System.in); //get Import from User
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(System.in)); //get Import from User
 
             Integer ui= Integer.valueOf(inputStream.readLine()); //User import into Integer
             outputStream.writeInt(ui);// Write user import into Stream objekt
-
+            //clientSocket.close();
         }catch(Exception exc){
             System.out.println("It did not work! \nException message: "+exc.getMessage());
         }
 
     }
-
-
-
-
-
 
 }
--- app/src/main/java/com/example/myapplication/Server.java
+++ app/src/main/java/com/example/myapplication/Server.java
@@ -1,4 +1,30 @@
 package com.example.myapplication;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args){
+        try{
+            ServerSocket serverSocket = new ServerSocket(53212);
+            Socket socket=serverSocket.accept(); //accept the client socket
+            System.out.println("It worked out! Connected! Juhu :)");
+
+            DataInputStream dataInputStream=new DataInputStream(socket.getInputStream()); //Data input stream
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(System.in));
+            int matrikelnr=dataInputStream.readInt();//read Matrikelnummer from inputStream
+            System.out.println("Client send message: "+matrikelnr);
+
+            //socket.close();
+
+
+        }catch(Exception exc){
+            System.out.println("It did not work! \nException message: "+exc.getMessage());
+        }
+
+
+    }
 }
