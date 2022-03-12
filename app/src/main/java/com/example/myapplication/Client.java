55db4cefe46fa3b1536d2c76f05fcb11494a1d1e
Your Name <vivienne.haferkamp@gmail.com>
2022-03-12 12:59:48 +0100

    changed client and server class

--- app/src/main/java/com/example/myapplication/Client.java
+++ app/src/main/java/com/example/myapplication/Client.java
@@ -1,23 +1,51 @@
 package com.example.myapplication;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
 
         try{
             Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
-            DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream()); //we convert the raw data into stream
-            BufferedReader inputStream = new BufferedReader(new InputStreamReader(System.in)); //get Import from User
+            InputStreamReader inputStreamReader=new InputStreamReader(clientSocket.getInputStream()); // read data from the server
+            //is a stream of bytes
+            OutputStreamWriter outputStreamWriter=new OutputStreamWriter(clientSocket.getOutputStream());  //output data to send msg to the server - output stream writer = bridge vom Byte Streams to Character Streams
+            //is a stream of bytes
 
-            Integer ui= Integer.valueOf(inputStream.readLine()); //User import into Integer
-            outputStream.writeInt(ui);// Write user import into Stream objekt
-            //clientSocket.close();
+            //So we wrap those two, to increase efficency
+            BufferedReader bufferedReader=new BufferedReader(inputStreamReader);//Wrap for more efficency - buffered reader
+            BufferedWriter bufferedWriter=new BufferedWriter(outputStreamWriter); //Wrap for more efficency
+
+            //Get the imput from keyboard
+            Scanner sc=new Scanner(System.in);
+            while (true){
+                //SEND MSG TO SERVER
+                String msgSend=sc.nextLine();
+                bufferedWriter.write(msgSend);
+                bufferedWriter.newLine(); //without new line it wouldnt know that the msg is over
+                bufferedWriter.flush(); //bufferedWriter should send each msg. not wait until its full
+
+                //WAIT FOR RESPONSE FROM SERVER
+                System.out.println("Server response: "+bufferedReader.readLine());
+
+                //END ENDLESS LOOP
+                if(msgSend.equalsIgnoreCase("0")){
+                    break;
+                }
+            }
+
+            clientSocket.close();
+
+
         }catch(Exception exc){
-            System.out.println("It did not work! \nException message: "+exc.getMessage());
+            System.out.println("It did not work! Exception: "+exc.getMessage().toString());
         }
 
     }
--- app/src/main/java/com/example/myapplication/Server.java
+++ app/src/main/java/com/example/myapplication/Server.java
@@ -1,28 +1,51 @@
 package com.example.myapplication;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args){
-        try{
-            ServerSocket serverSocket = new ServerSocket(53212);
-            Socket socket=serverSocket.accept(); //accept the client socket
-            System.out.println("It worked out! Connected! Juhu :)");
+        //For creating a new socket every time the client excepts the connection
+        while (true) {
+            try {
+                ServerSocket serverSocket = new ServerSocket(53212); //waits for requests
+                Socket socket = serverSocket.accept(); //accept the client socket --> WE CAN CONNECT NOW
+                System.out.println("It worked out! Connected! Juhu :)");
 
-            DataInputStream dataInputStream=new DataInputStream(socket.getInputStream()); //Data input stream
-            BufferedReader inputStream = new BufferedReader(new InputStreamReader(System.in));
-            int matrikelnr=dataInputStream.readInt();//read Matrikelnummer from inputStream
-            System.out.println("Client send message: "+matrikelnr);
+                InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+                //WRAP FOR MORE EFFICENCY
+                BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-            //socket.close();
+                while (true) { //use to send data back and forward
+                    String msgFromClient = bufferedReader.readLine();
+                    System.out.println("Client send message: " + msgFromClient);
+                    String antwort = "";
+                    bufferedWriter.write(antwort);
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush(); //otherwise bufferdwriter would wait till its full
+
+                    if (msgFromClient.equals("0")) {
+                        break;
+                    }
+                }
+
+                socket.close();
+                inputStreamReader.close();
+                bufferedWriter.close();
+                bufferedReader.close();
+                outputStreamWriter.close();
 
 
-        }catch(Exception exc){
-            System.out.println("It did not work! \nException message: "+exc.getMessage());
+            } catch (IOException exc) {
+                System.out.println("It did not work! \nException message: " + exc.getMessage());
+            }
         }
 
 
