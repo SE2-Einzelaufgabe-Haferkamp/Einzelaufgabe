fee09c510d900a702526a3643291763388588da5
Your Name <vivienne.haferkamp@gmail.com>
2022-03-12 11:18:44 +0100

    Client Class

---
+++ app/src/main/java/com/example/myapplication/Client.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args){
+
+        try{
+            Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+            DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream()); //we convert the raw data into stream
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(System.in); //get Import from User
+
+            Integer ui= Integer.valueOf(inputStream.readLine()); //User import into Integer
+            outputStream.writeInt(ui);// Write user import into Stream objekt
+
+        }catch(Exception exc){
+            System.out.println("It did not work! \nException message: "+exc.getMessage());
+        }
+
+    }
+
+
+
+
+
+
+}
