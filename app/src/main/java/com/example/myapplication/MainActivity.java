f2e384e185f21aa70e481229ee82d6dd36cd1f2b
Your Name <vivienne.haferkamp@gmail.com>
2022-03-13 09:26:32 +0100

    Client & Server CLass zusammenfassen (Server unnötig)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,74 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+        Button berechnen = findViewById(R.id.button2);
+        TextView serverAnswer = findViewById(R.id.textView);
+        EditText inputNumber = findViewById(R.id.editTextNumber);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serverAnswer.setVisibility(View.VISIBLE);
+                String matrikelnummer = inputNumber.getText().toString();
+                Connection con = new Connection(matrikelnummer);
+
+                con.start();
+                try {
+                    con.join();
+                } catch (InterruptedException e) {
+                }
+
+                serverAnswer.setText(con.getAnswer());
+            }
+
+        });
+        berechnen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                serverAnswer.setVisibility(View.VISIBLE);
+                String matrikelnummer = inputNumber.getText().toString();
+                Connection con = new Connection(matrikelnummer);
+
+                con.start();
+                try {
+                    con.join();
+                } catch (InterruptedException e) {
+                }
+                if(!con.getAnswer().equals("Dies ist keine gueltige Matrikelnummer")){
+                    berechnen(matrikelnummer);
+                }
+                else{
+                    serverAnswer.setText("Das ist keine Matrikelnummer");
+                }
+            }
+        });
     }
+    public void berechnen(String matNr) {
+        int firstSum = 0;
+        int secondSum = 0;
+        int[] numbers = new int[matNr.length()];
+        char[] c = matNr.toCharArray();
+
+        for (int i = 0; i < matNr.length(); i++) {
+            numbers[i] = (int) c[i]-48;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 == 0) {
+                firstSum += numbers[i];
+            } else {
+                secondSum += numbers[i];
+            }
+        }
+        int alternierendeQuersumme = firstSum-secondSum;
+        if(alternierendeQuersumme%2==0){
+            serverAnswer.setText("GERADE");
+        }
+        else{
+            serverAnswer.setText("UNGERADE");
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/example/myapplication/Network.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Network extends Thread{
+    String userInput;
+    String serverAnswer;
+
+    public Network(){userInput="0";}
+    public Network(String input){userInput=input;}
+    public String returnAnswer(){return this.serverAnswer;}
+
+    @Override
+    public void run(){
+
+        try{
+            Socket socket = new Socket("se2-isys.aau.at", 53212);
+            BufferedReader serverAnswerStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //wrap into Burffered Reader for more efficency
+            DataOutputStream serverInput=new DataOutputStream(socket.getOutputStream());
+            //fill with user input
+
+            serverInput.writeBytes( userInput+ "\n" );
+            serverAnswer=serverAnswerStream.readLine();
+
+            socket.close();
+
+        }catch(Exception exc){
+            System.out.println("It did not work! Exception: "+exc.getMessage().toString());
+        }
+    }
+
+
+
+}
